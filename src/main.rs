dbdf0aa7da8f50d402d23c8d842eafa64068c92a
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-11-02 12:16:01 +0000

    Take user input and trim it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rustyline::error::ReadlineError;
+use rustyline::{Editor};
+
 fn main() {
-    println!("Hello, world!");
+    // `()` can be used when no completer is required
+    let mut rl = match Editor::<()>::new() {
+        Ok(rl) => rl,
+        Err(error) => panic!("Problem with readline: {:?}", error),
+    };
+
+    println!("Please input a github username below:");
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                let user_name = line.as_str().trim();
+
+                println!("Line: {}", user_name);
+                break
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
 }
